9a71e317bc23cc389453ba2fb5dc496e34411073
Fernando J. Pereda <ferdy@posidon.ferdyx.org>
2006-04-01 14:20:45 +0200

    Introduce lib/colors.sh
    This script is to be sourced by repodoc to do colorized
    output. Please don't abuse colors.
    
    Signed-off-by: Fernando J. Pereda <ferdy@gentoo.org>

---
+++ lib/colors.sh
@@ -0,0 +1,29 @@
+#
+# Some nice colors to source in repodoc
+#
+RED="\033[01;31m"
+GREEN="\033[01;32m"
+YELLOW="\033[01;33m"
+BLUE="\033[01;34m"
+WHITE="\033[01;38m"
+OFF="\033[00m"
+
+red() {
+	echo -e ${RED}$*${OFF}
+}
+
+green() {
+	echo -e ${GREEN}$*${OFF}
+}
+
+yellow() {
+	echo -e ${YELLOW}$*${OFF}
+}
+
+blue() {
+	echo -e ${BLUE}$*${OFF}
+}
+
+white() {
+	echo -e ${BLUE}$*${OFF}
+}
