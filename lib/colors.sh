8ad4ca1f6654b16447fe3abe435fbd7295d64680
Fernando J. Pereda <ferdy@posidon.ferdyx.org>
2006-04-14 18:27:36 +0200

    Disable colors when we are not printing to a tty.
    Signed-off-by: Fernando J. Pereda <ferdy@gentoo.org>

--- lib/colors.sh
+++ lib/colors.sh
@@ -1,12 +1,22 @@
 #
 # Some nice colors to source in repodoc
 #
-RED="\033[01;31m"
-GREEN="\033[01;32m"
-YELLOW="\033[01;33m"
-BLUE="\033[01;34m"
-WHITE="\033[01;38m"
-OFF="\033[00m"
+
+if tty -s <&1 ; then
+	RED="\033[01;31m"
+	GREEN="\033[01;32m"
+	YELLOW="\033[01;33m"
+	BLUE="\033[01;34m"
+	WHITE="\033[01;38m"
+	OFF="\033[00m"
+else
+	RED=
+	GREEN=
+	YELLOW=
+	BLUE=
+	WHITE=
+	OFF=
+fi
 
 red() {
 	echo -e ${RED}$*${OFF}
