472101741be47424428b1a0cf4982ec4941f981d
Fernando J. Pereda <ferdy@posidon.ferdyx.org>
2006-05-10 00:13:15 +0200

    Add a function to translate GuideXML anchors into XPath expressions.
    Signed-off-by: Fernando J. Pereda <ferdy@gentoo.org>

--- lib/xpath.sh
+++ lib/xpath.sh
@@ -5,3 +5,20 @@
 doxpath() {
 	${EXTRA_DIR}/xpatheval $* 2>/dev/null
 }
+
+# Specify a GuideXML thingie and it will give you
+# a proper XPath expresion ready to be used.
+anchor2xpath() {
+	local myanch=${1}
+
+	myanch=${myanch//\#doc_/\/\/}
+	myanch=${myanch//chap/chapter\[}
+	myanch=${myanch//sect/section\[}
+	myanch=${myanch//pre/descendant::pre\[}
+	myanch=${myanch//fig/descendant::fig\[}
+	myanch=${myanch//_/\]\/}
+	myanch="${myanch}]"
+	myanch=${myanch//\[\]/\[1\]}
+
+	echo ${myanch}
+}
