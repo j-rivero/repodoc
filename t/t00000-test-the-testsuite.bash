3796a061f5d586b5d6314446dd63e43a15e8da7f
Fernando J. Pereda <ferdy@posidon.ferdyx.org>
2006-08-13 17:44:37 +0200

    t: Check that we set FILESDIR properly.
    Signed-off-by: Fernando J. Pereda <ferdy@gentoo.org>

--- t/t00000-test-the-testsuite.bash
+++ t/t00000-test-the-testsuite.bash
@@ -10,4 +10,7 @@ test_expect_ok 'Ok should be 0' 'echo 0'
 test_expect_warning 'Warning should be 1' 'echo 1'
 test_expect_error 'Error should be 2' 'echo 2'
 
+# Check variables
+test_expect_pass '${FILESDIR} == t00000' "[[ ${FILESDIR} == t00000 ]]"
+
 test_done
